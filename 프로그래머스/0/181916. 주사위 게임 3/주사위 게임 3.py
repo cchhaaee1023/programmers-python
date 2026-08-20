41e8dc9a6bfee2e0c323d39ff91a4290156c65f8
cchhaaee1023 <chaeyeong5601@gmail.com>
2026-08-20 12:29:26 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -2,24 +2,12 @@ def solution(a, b, c, d):
     dice = sorted([a,b,c,d])
     count_same = 0
 
-    uniq = list(set(dice))
-    
-    
-    match len(uniq):
-        case 1:
-            count_same = 4
-        case 2:
-            count_same = 2
-        case 3:
-            count_same = 1
-        case 4:
-            count_same = 0
-            
+    uniq = list(set(dice))  
 
-    if count_same == 4:
+    if len(uniq) == 1:
         return 1111*a
     
-    elif count_same == 2:
+    elif len(uniq) == 2:
         if dice.count(dice[0]) == 3:
             p = dice[0]
             q = dice[3]
@@ -28,17 +16,15 @@ def solution(a, b, c, d):
             q = dice[0]
         else:
             return (uniq[0] + uniq[1]) * abs(uniq[0] - uniq[1])
-        
         return (10 * p + q)**2
     
-    elif count_same == 1:
+    elif len(uniq) == 3:
         for u in uniq:
             if dice.count(u) == 2:
                 uniq.remove(u)
-                # uniq.remove(u)
         return uniq[0] * uniq[1]
     
-    else:
+    elif len(uniq) == 4:
         return min(dice)
         
         
